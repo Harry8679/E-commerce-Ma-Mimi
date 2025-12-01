3a5935f067eef8c0c2f385246e8e17ecc3db6b60
Harry7986 <emarh.harry.code@gmail.com>
2025-12-01 21:43:35 +0100

    Webpack 2

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,16 @@ Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
-    .enableSassLoader()
+    
+    // Activer PostCSS pour Tailwind
     .enablePostCssLoader()
+    
     .splitEntryChunks()
     .enableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     .enableVersioning(Encore.isProduction())
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.23';
-    })
 ;
 
 module.exports = Encore.getWebpackConfig();
