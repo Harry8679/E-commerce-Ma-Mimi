06acc458fa506dde87fd949d6880f4c96e0cd658
Harry7986 <emarh.harry.code@gmail.com>
2025-12-03 08:20:12 +0100

    Dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,49 +2,61 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use App\Entity\Address;
+use App\Entity\Carrier;
+use App\Entity\Category;
+use App\Entity\Invoice;
+use App\Entity\Order;
+use App\Entity\OrderItem;
+use App\Entity\Payment;
+use App\Entity\Product;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-#[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        // 1.1) If you have enabled the "pretty URLs" feature:
-        // return $this->redirectToRoute('admin_user_index');
-        //
-        // 1.2) Same example but using the "ugly URLs" that were used in previous EasyAdmin versions:
-        // $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        // return $this->redirect($adminUrlGenerator->setController(OneOfYourCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirectToRoute('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
+        // Redirection vers la liste des commandes par défaut
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(OrderCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('E Commerce Rhum');
+            ->setTitle('🥃 Rhum Shop - Administration')
+            ->setFaviconPath('data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>🥃</text></svg>');
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+
+        yield MenuItem::section('E-commerce');
+        yield MenuItem::linkToCrud('Commandes', 'fas fa-shopping-cart', Order::class);
+        yield MenuItem::linkToCrud('Produits', 'fas fa-wine-bottle', Product::class);
+        yield MenuItem::linkToCrud('Catégories', 'fas fa-tags', Category::class);
+
+        yield MenuItem::section('Livraison');
+        yield MenuItem::linkToCrud('Transporteurs', 'fas fa-truck', Carrier::class);
+
+        yield MenuItem::section('Clients');
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', User::class);
+        yield MenuItem::linkToCrud('Adresses', 'fas fa-map-marker-alt', Address::class);
+
+        yield MenuItem::section('Facturation');
+        yield MenuItem::linkToCrud('Paiements', 'fas fa-credit-card', Payment::class);
+        yield MenuItem::linkToCrud('Factures', 'fas fa-file-invoice', Invoice::class);
+
+        yield MenuItem::section('Retour au site');
+        yield MenuItem::linkToRoute('Voir le site', 'fas fa-eye', 'app_home');
+        yield MenuItem::linkToLogout('Déconnexion', 'fas fa-sign-out-alt');
     }
 }
