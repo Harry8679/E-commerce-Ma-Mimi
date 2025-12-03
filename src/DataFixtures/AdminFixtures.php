1fd98d206df5493f14d8f868474745791abad0eb
Harry7986 <emarh.harry.code@gmail.com>
2025-12-03 08:34:22 +0100

    Admin Fixtures 2

--- src/DataFixtures/AdminFixtures.php
+++ src/DataFixtures/AdminFixtures.php
@@ -2,16 +2,31 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AdminFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $admin = new User();
+        $admin->setEmail('admin@rhum-shop.com');
+        $admin->setFirstName('Admin');
+        $admin->setLastName('RHUM SHOP');
+        $admin->setRoles(['ROLE_ADMIN']);
+        $admin->setIsVerified(true);
+        $admin->setPassword($this->passwordHasher->hashPassword($admin, 'admin123'));
 
+        $manager->persist($admin);
         $manager->flush();
     }
 }
