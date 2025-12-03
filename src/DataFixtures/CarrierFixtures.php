5f3a8054042960f698d4f7f182397a56af6e0491
Harry7986 <emarh.harry.code@gmail.com>
2025-12-03 07:36:12 +0100

    CarrierFixtures 2

--- src/DataFixtures/CarrierFixtures.php
+++ src/DataFixtures/CarrierFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Carrier;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,48 @@ class CarrierFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $carriers = [
+            [
+                'name' => 'Colissimo',
+                'description' => 'Livraison à domicile par La Poste avec suivi',
+                'price' => '6.90',
+                'deliveryTime' => '2-3 jours ouvrés',
+                'position' => 1,
+            ],
+            [
+                'name' => 'Chronopost',
+                'description' => 'Livraison express en 24h',
+                'price' => '12.90',
+                'deliveryTime' => '24h',
+                'position' => 2,
+            ],
+            [
+                'name' => 'Mondial Relay',
+                'description' => 'Livraison en point relais',
+                'price' => '4.90',
+                'deliveryTime' => '3-5 jours ouvrés',
+                'position' => 3,
+            ],
+            [
+                'name' => 'La Poste - Lettre Suivie',
+                'description' => 'Envoi en lettre suivie (petits colis uniquement)',
+                'price' => '3.50',
+                'deliveryTime' => '3-4 jours ouvrés',
+                'position' => 4,
+            ],
+        ];
+
+        foreach ($carriers as $carrierData) {
+            $carrier = new Carrier();
+            $carrier->setName($carrierData['name']);
+            $carrier->setDescription($carrierData['description']);
+            $carrier->setPrice($carrierData['price']);
+            $carrier->setDeliveryTime($carrierData['deliveryTime']);
+            $carrier->setPosition($carrierData['position']);
+            $carrier->setIsActive(true);
+
+            $manager->persist($carrier);
+        }
 
         $manager->flush();
     }
