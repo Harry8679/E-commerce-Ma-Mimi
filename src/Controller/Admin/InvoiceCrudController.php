c07d52a2c41c1d408cb1ef820fe85e023c372deb
Harry7986 <emarh.harry.code@gmail.com>
2025-12-03 08:29:35 +0100

    Invoice Admin

--- src/Controller/Admin/InvoiceCrudController.php
+++ src/Controller/Admin/InvoiceCrudController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Invoice;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class InvoiceCrudController extends AbstractCrudController
@@ -15,14 +19,28 @@ class InvoiceCrudController extends AbstractCrudController
         return Invoice::class;
     }
 
-    /*
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Facture')
+            ->setEntityLabelInPlural('Factures')
+            ->setDefaultSort(['invoiceDate' => 'DESC'])
+            ->setPageTitle('index', 'Liste des factures');
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::NEW, Action::EDIT, Action::DELETE);
+    }
+
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->onlyOnIndex();
+        yield TextField::new('invoiceNumber', 'Numéro');
+        yield AssociationField::new('order', 'Commande');
+        yield DateTimeField::new('invoiceDate', 'Date');
+        yield TextField::new('pdfPath', 'Fichier PDF')->hideOnIndex();
+        yield DateTimeField::new('createdAt', 'Créé le')->hideOnForm();
     }
-    */
 }
