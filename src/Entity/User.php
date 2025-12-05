6afb42a549eedb163aacdc1a05aa61bcc7de9fd8
Harry7986 <emarh.harry.code@gmail.com>
2025-12-05 05:19:18 +0100

    User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -263,4 +263,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         }
         return $this;
     }
+
+    public function __toString(): string
+    {
+        return $this->getFullName() ?? $this->email ?? 'Utilisateur';
+    }
 }
