61a0c0e21ce63eb78334b9fdf10f400633897544
Harry7986 <emarh.harry.code@gmail.com>
2025-12-05 04:50:57 +0100

    Order Controller 2

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,17 +2,75 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Entity\User;
+use App\Repository\OrderRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
-final class OrderController extends AbstractController
+#[Route('/mon-compte/commandes')]
+class OrderController extends AbstractController
 {
-    #[Route('/order', name: 'app_order')]
+    public function __construct(
+        private OrderRepository $orderRepository
+    ) {
+    }
+
+    /**
+     * Récupérer l'utilisateur connecté avec le bon typage
+     */
+    private function getAuthenticatedUser(): User
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+        
+        if (!$user instanceof User) {
+            throw $this->createAccessDeniedException('Vous devez être connecté pour accéder à cette page.');
+        }
+        
+        return $user;
+    }
+
+    /**
+     * Liste des commandes de l'utilisateur
+     */
+    #[Route('', name: 'app_account_orders')]
     public function index(): Response
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getAuthenticatedUser();
+        
+        // Récupérer toutes les commandes de l'utilisateur, triées par date décroissante
+        $orders = $this->orderRepository->findBy(
+            ['user' => $user],
+            ['createdAt' => 'DESC']
+        );
+
+        return $this->render('account/orders/index.html.twig', [
+            'orders' => $orders,
+        ]);
+    }
+
+    /**
+     * Détail d'une commande
+     */
+    #[Route('/{id}', name: 'app_account_order_show')]
+    public function show(int $id): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getAuthenticatedUser();
+        $order = $this->orderRepository->find($id);
+
+        // Vérifier que la commande existe et appartient bien à l'utilisateur
+        if (!$order || $order->getUser() !== $user) {
+            throw $this->createNotFoundException('Commande introuvable');
+        }
+
+        return $this->render('account/orders/show.html.twig', [
+            'order' => $order,
         ]);
     }
 }
